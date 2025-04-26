7731fd42230b901fa39d127fb78876fd456296dd
Cheng Soo Tan <chtan@u90z04s02.42kl.edu.my>
2025-04-26 15:36:11 +0800

    fix: modified ex01

--- ex01/inc/ScavTrap.hpp
+++ ex01/inc/ScavTrap.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: chtan <chtan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 12:05:36 by chtan             #+#    #+#             */
-/*   Updated: 2025/04/18 12:07:40 by chtan            ###   ########.fr       */
+/*   Updated: 2025/04/26 15:33:57 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,25 @@
 # include <iostream>
 # include "ClapTrap.hpp"
 
-class	ScavTrap : virtual public ClapTrap
+class	ScavTrap: virtual public ClapTrap
 {
 
-public:
+	public:
+		ScavTrap(std::string name);
+		ScavTrap(ScavTrap const & input);
+		ScavTrap & operator=(ScavTrap const & input);
+		~ScavTrap(void);
 
-	ScavTrap(std::string name);
-	ScavTrap(ScavTrap const & input);
-	ScavTrap & operator=(ScavTrap const & input);
-	~ScavTrap(void);
+		void	attack(const std::string & target);
+		void	guardGate(void);
 
-	void	attack(const std::string & target);
-	void	guardGate(void);
+	private:
+		void	status(void);
 
-private:
-
-	void	status(void);
-
-protected:
-
-	static unsigned int const _initHitPoints = 100;
-	static unsigned int const _initEnergyPoints = 50;
-	static unsigned int const _initAttackDamage = 20;
+	protected:
+		static unsigned int const _initHitPoints = 100;
+		static unsigned int const _initEnergyPoints = 50;
+		static unsigned int const _initAttackDamage = 20;
 
 };
 
