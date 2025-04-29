a7da583cd5a6f314678ed90eda459668fea8bb76
Jaxz Tan <jaxztan@Jaxzs-MacBook-Pro.local>
2025-04-29 21:09:45 +0800

    fix: did a simple test case

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,43 +6,69 @@
 /*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 12:09:00 by chtan             #+#    #+#             */
-/*   Updated: 2025/04/18 12:09:11 by chtan            ###   ########.fr       */
+/*   Updated: 2025/04/29 21:04:54 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ClapTrap.hpp"
 #include "../inc/ScavTrap.hpp"
 
-int	main(void)
+int main(void)
 {
-	ClapTrap	trapA("Hugo");
-	ScavTrap	trapB("Tom");
+	std::cout << "### SIMPLE TEST WITH CLAPTRAP & SCAVTRAP ###" << std::endl;
 
+	ClapTrap	trapA("Alice");
+	ScavTrap	trapB("Bob");
+
+	std::cout << "\n-- ScavTrap special mode --" << std::endl;
 	trapB.guardGate();
-	trapA.attack(trapB.getName());
-	trapB.takeDamage(20);
-	trapA.attack(trapB.getName());
-	trapB.takeDamage(20);
-	trapB.attack(trapA.getName());
-	trapA.takeDamage(7);
-	trapA.beRepaired(5);
-	trapB.attack(trapA.getName());
-	trapA.takeDamage(5);
-	trapA.beRepaired(5);
-	trapB.attack(trapA.getName());
-	trapA.takeDamage(2);
-	trapA.attack(trapB.getName());
-	trapB.takeDamage(2);
-	trapB.attack(trapA.getName());
-	trapA.takeDamage(7);
-	trapA.attack(trapA.getName());
-	trapA.takeDamage(5);
-	trapA.beRepaired(3);
-	trapB.attack(trapA.getName());
-	trapA.takeDamage(2);
-	trapA.attack(trapB.getName());
-	trapB.beRepaired(5);
-	trapA.beRepaired(3);
 
-	return (0);
+	std::cout << "\n-- ClapTrap attacks ScavTrap --" << std::endl;
+	trapA.attack(trapB.getName());
+	trapB.takeDamage(5);
+
+	std::cout << "\n-- ScavTrap attacks ClapTrap --" << std::endl;
+	trapB.attack(trapA.getName());
+	trapA.takeDamage(7);
+
+	std::cout << "\n-- Both repair themselves --" << std::endl;
+	trapA.beRepaired(4);
+	trapB.beRepaired(6);
+
+	std::cout << "\n### TEST END ###" << std::endl;
+	return 0;
 }
+
+// int	main(void)
+// {
+// 	ClapTrap	trapA("Hugo");
+// 	ScavTrap	trapB("Tom");
+
+// 	trapB.guardGate();
+// 	trapA.attack(trapB.getName());
+// 	trapB.takeDamage(20);
+// 	trapA.attack(trapB.getName());
+// 	trapB.takeDamage(20);
+// 	trapB.attack(trapA.getName());
+// 	trapA.takeDamage(7);
+// 	trapA.beRepaired(5);
+// 	trapB.attack(trapA.getName());
+// 	trapA.takeDamage(5);
+// 	trapA.beRepaired(5);
+// 	trapB.attack(trapA.getName());
+// 	trapA.takeDamage(2);
+// 	trapA.attack(trapB.getName());
+// 	trapB.takeDamage(2);
+// 	trapB.attack(trapA.getName());
+// 	trapA.takeDamage(7);
+// 	trapA.attack(trapA.getName());
+// 	trapA.takeDamage(5);
+// 	trapA.beRepaired(3);
+// 	trapB.attack(trapA.getName());
+// 	trapA.takeDamage(2);
+// 	trapA.attack(trapB.getName());
+// 	trapB.beRepaired(5);
+// 	trapA.beRepaired(3);
+
+// 	return (0);
+// }
